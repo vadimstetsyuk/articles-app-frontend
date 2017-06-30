9a9a4eb9e3f931b44a564f4f503a4297641c32f9
Вадим Стецюк < >
2017-07-01 01:54:52 +0300

    Added articles.service

---
+++ src/app/services/article.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions, Response } from '@angular/http';
+import { Observable } from 'rxjs';
+import 'rxjs/add/operator/map'
+
+import { Article } from '../models/article';
+
+@Injectable()
+export class ArticleService {
+    constructor(
+        private http: Http) {
+    }
+
+    getArticles(): Observable<Article[]> {
+        let url = 'http://localhost:3000/api/articles';
+
+        return this.http.get(url)
+            .map((res: Response) => res.json())
+            .catch((error: any) => Observable.throw(error.json().error || 'Server error'));
+    }
+}
