e1c77fb56b9d87c1c74d205874f86c4c0e5ae95e
Вадим Стецюк < >
2017-06-30 20:58:28 +0300

    Created Comment model

---
+++ src/app/models/comment.ts
@@ -0,0 +1,12 @@
+/*
+    The class which represents object of Comment
+*/
+
+export class Comment {
+    id: Number;
+    articleId: Number;
+    senderName: String;
+    senderEmail: String;
+    date: Date;
+    description: String;
+}
