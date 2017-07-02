3a5d0514ca234a5e9d6d45d23a38e58f89980ec4
Вадим Стецюк < >
2017-07-02 03:22:14 +0300

    Added logout for toolbar component if user was not logged

--- src/app/toolbar/toolbar.component.ts
+++ src/app/toolbar/toolbar.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, AfterContentChecked } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../services/index';
+import { AuthGuard } from '../guards/auth.guard';
 
 @Component({
     selector: 'toolbar',
@@ -6,6 +9,21 @@ import { Component } from '@angular/core';
     styleUrls: ['toolbar.component.scss']
 })
 
-export class ToolbarComponent {
+export class ToolbarComponent implements AfterContentChecked {
+    isLogged: boolean;
 
+    constructor(private authenticationService: AuthenticationService,
+                private authGuard: AuthGuard,
+                private router: Router) {
+                    this.isLogged = false;
+                }
+
+    ngAfterContentChecked() {
+        this.router.url == '/login' ? this.isLogged = false : this.isLogged = true;
+    }
+
+    logout() {
+        this.authenticationService.logout();
+        this.router.navigate(['/login']);
+    }
 }
